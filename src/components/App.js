d4ba5f50fb2eaf4a3d5f053cc382571e13e75fd9
Jared Patterson <Jaredp75@hotmil.com>
2018-02-13 10:26:46 -0500

    App.js work

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import '../styles/App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from '../components/Home';
+import About from '../components/About';
 import logo from './msair100.jpg';
 import './App.css';
 
@@ -13,6 +17,14 @@ class App extends Component {
         <p className="App-intro">
           Your source for compressed air vacuum systems for medical, dental, laboratory and industrial applications
         </p>
+        <BrowserRouter>
+          <Baselayout>
+            <Switch>
+              <Route exact path = "/" component={Home}/>
+              <Route path = "about" component ={About}/>
+            </Switch>
+          </Baselayout>
+        </BrowserRouter>
       </div>
     );
   }
