ba53937b740ad5146def3cf73cd390bf04ff8a5c
Jared Patterson <Jaredp75@hotmil.com>
2018-03-05 16:22:11 -0500

    Component and CSS work

--- src/components/App.js
+++ src/components/App.js
@@ -15,19 +15,37 @@ import logo from '../msair100.jpg';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Your source for compressed air vacuum systems for medical, dental, laboratory and industrial applications</h1>
         </header>
-        {/* <p className="App-intro">
-          Your source for compressed air vacuum systems for medical, dental, laboratory and industrial applications
-        </p> */}
+        {/* < className="search-header"> */}
+          <div className="search-wrapper">
+          <div className="search">
+            <input type="text" class="searchTerm" placeholder="Search the site..."></input>
+              <button type="submit" className="searchButton">
+                <i className="fa fa-search"></i>
+              </button>
+            </div>
+        </div>
+          {/* <form className="search" action="/action_page.php" style="margin:auto;max-width:300px">
+          <div>
+            <input type="text" placeholder="Search.." name="search2"></input>
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+          </form> */}
+
+
+
+
+
 
         <div className="wrapper">
           <div className="contactnav">
               <nav id="contactphone">
-                <i><strong>Toll Free: 1-877-MSAIR99</strong></i>
+                <i><strong>Call Us Toll Free: 1-877-MSAIR99</strong></i>
               </nav>
           </div>
           <header id="site-header" class="body">
@@ -39,9 +57,14 @@ class App extends Component {
             <a href="about.html">ABOUT US</a>
           </nav>
         </header>
-        </div>
+      </div>
+
         <div className="checkout-wrapper">
+          <nav id="social-nav">
+            <strong>Follow Us:</strong> <i class="fab fa-facebook-square"></i> | <i class="fab fa-twitter-square"></i>
+          </nav>
           <header id="site-header" class="body">
+
             <nav id="main-navigation">
               <a href="contact.html">Contact Us</a> |
               <a href="myaccount.html">My Account</a> |
@@ -49,6 +72,7 @@ class App extends Component {
             </nav>
           </header>
         </div>
+
 
         <BrowserRouter>
           <Baselayout>
@@ -63,7 +87,7 @@ class App extends Component {
             </Switch>
           </Baselayout>
         </BrowserRouter>
-      </div>
+        </div>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -26,13 +26,13 @@ export default class Home extends React.Component {
     console.log(this.state.tourList)
     return(
       <div>
-        <div className = "tour-main-photo" background-image={this.state.orgInfo.org_photo}>
+        <div className = "content-area-container">
           <div className = "tour-main-text">
             <div><h1>{this.state.orgInfo.display_name}</h1></div>
             <div><h2>{this.state.orgInfo.description}</h2></div>
           </div>
           <p>Having trouble locating a part? Contact us using our web submission form <a href="http://www.msaironline.com/contact/contact.php?linkid=message" target="_blank" rel="noopener noreferrer">here.</a> Or call <strong>1-877-MSAIR99</strong>.</p>
-        </div>
+
 
         <div className = "tour-item-coming">
           <div className = "tour-item-text">
@@ -105,12 +105,6 @@ export default class Home extends React.Component {
             <p>Solmetex</p>
           </div>
         </div>
-
-
-
-
-
-
         <div className = "select-a-tour">
           <h3>TOP SELLERS</h3>
         </div>
@@ -127,23 +121,27 @@ export default class Home extends React.Component {
           </div>
         })}
 
+</div>
 
-      </div>
+
 
       <div className="Footer">
         <FooterLinks />
       </div>
-
+    </div>
 
       <div className="about-footer">
         <div className="terms">
-          <p><a href="/terms" target=" blank" rel="noopener noreferrer">Terms of Use</a> | <a href="/privacy" target='_blank' rel="noopener noreferrer">Privacy Policy</a></p>
+          <p><a href="/terms" target="_blank" rel="noopener noreferrer">Terms of Use</a> | <a href="/privacy" target='_blank' rel="noopener noreferrer">Privacy Policy</a></p>
         </div>
         <div className="copyright">
           <p>&copy; 2018 - MS Air, Inc. | <Link to="/">Home</Link></p>
       </div>
       </div>
     </div>
+
+
+
     )
   }
 }
